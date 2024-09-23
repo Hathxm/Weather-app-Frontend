caa03790a2115c9cd601b855f4f73a35d1d78511
hathim <hathim@mail.com>
2024-09-23 12:01:16 +0530

    base url fixed

--- src/axiosinstance/axiosinstance.js
+++ src/axiosinstance/axiosinstance.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const BASE_URL = process.env.REACT_APP_BASE_URL
 
 const axiosInstance = axios.create({
-    baseURL: 'http://127.0.0.1:8000/',
+    baseURL: BASE_URL,
     // Add other configurations like timeout, headers, etc., if needed
 });
 
